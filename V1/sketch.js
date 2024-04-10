097684c75465781fce7bcca01945073ee1638faf
Matheus Pacheco <70149366+killtheweb@users.noreply.github.com>
2024-04-10 17:16:36 -0300

    Update sketch.js
    mp4 config

--- V1/sketch.js
+++ V1/sketch.js
@@ -12,23 +12,56 @@ let checkbox;
 let fontMin;
 let animSpeed;
 
-let recording = false;
 let startTime;
+
+
+const ancho_inicial = 1080;
+const alto_inicial = 1080;
+
+let botón_de_grabación;
+let encoder;
+let recording = false;
 
 
 function preload() {
   img = loadImage(string);
+    img.willReadFrequently = true; // Setting willReadFrequently to true
+
   BOLDD = loadFont("FLAVOR.otf");
   pg = createGraphics(width, height);
 
+  HME.createH264MP4Encoder().then(enc => {
+    encoder = enc;
+    encoder.outputFilename = (random() + "").replace("0.", "flvr");
+    encoder.width = ancho_inicial;
+    encoder.height = alto_inicial;
+    encoder.frameRate = 7;
+    encoder.kbps = 50000;
+    encoder.groupOfPictures = 10;
+    encoder.initialize();
+})
+  
 }
 
 function setup() {
 
-  cnv = createCanvas(1080,1080,WEBGL);
+
+  cnv = createCanvas(ancho_inicial,alto_inicial, WEBGL);
   frameRate(24);
 
   cnv.parent('output');
+
+  botón_de_grabación = createButton('Gravar')
+  botón_de_grabación.mousePressed(() => {
+       recording = botón_de_grabación.html() == "Gravar"
+       if (recording) {
+            botón_de_grabación.html("Parar")
+       } else {
+            botón_de_grabación.html("Gravar")
+            finalizarGrabación()
+       }
+
+  })
 
   checkbox = createCheckbox(' auto');
   checkbox.position(925, 650);
@@ -48,7 +81,7 @@ function setup() {
   animSpeed = createInput(4, int);
   animSpeed.position(1150,650);
 
-  pixelDensity(displayDensity);
+  pixelDensity(2);
   pg = createGraphics(width, height);
   laranja = color('#F84C01'); // Initialize laranja as orange
   cinza = color('#A3A3A3');   // Initialize cinza as gray
@@ -67,6 +100,8 @@ function setup() {
   vid = createVideo('your_video_file.mp4', vidLoaded);
   vid.hide(); // Hide the video element
 
+  
+
 }
 
 function vidLoaded() {
@@ -76,6 +111,7 @@ function vidLoaded() {
 
 
 function draw() {
+  
  
   background(0);
 
@@ -114,16 +150,20 @@ function draw() {
 
       pg.push();
       pg.textFont(BOLDD);
-      pg.textSize(b / map(sinMappedBrightness, 0, 255, 4, minSize));
+      pg.textSize(b / map(sinMappedBrightness, 0, 255, 6, minSize));
       pg.translate(x * TILE_W, y * TILE_H);
       // pg.text(CHARS.charAt(map(sin(radians(frameCount + x * y)), -1, 1, 0, CHARS.length - 1)), 0, 0);
       pg.text(CHARS.charAt(random(CHARS.length)),0,0)
       pg.pop();
+
     }
   }
   // updatePixels();
+  // scale(2);
 
-  image(pg, -width/2, -height/2, width, height);
+image(pg, -width/2, -height/2, encoder.width, encoder.height);
+
+  grabarFotograma();
 
 }
 
@@ -148,15 +188,19 @@ function gotFile(file) {
   if (file.type === 'image') {
     img = loadImage(file.data, () => {
       resizeCanvas(img.width, img.height)
+
       img.resize(TILES_X, TILES_Y);
+ 
     });
   } else if (file.type === 'video') {
     vid = createVideo(file.data, () => {
       // resizeCanvas(vid.width,vid.height);
       vid.size(TILES_X, TILES_Y);
+    
       vid.loop();
       vid.hide();
       img = vid;
+
       img.volume(0); // Mute the video if audio is not needed
     });
   }
@@ -164,5 +208,46 @@ function gotFile(file) {
 
 function resizeC() {
   resizeCanvas(1920, 1080);
+
+}
+
+
+function grabarFotograma() {
+  
+  if (recording) {
+    let imageData = get(0, 0, encoder.width, encoder.height).canvas.toDataURL("image/jpeg", 1.0);
+    let img = new Image();
+    img.src = imageData;
+    img.onload = function() {
+      let canvas = document.createElement('canvas');
+      canvas.width = encoder.width;
+      canvas.height = encoder.height;
+      let ctx = canvas.getContext('2d');
+      ctx.drawImage(img, 0, 0, encoder.width, encoder.height); // Draw with the correct size
+      let imageData = ctx.getImageData(0, 0, encoder.width, encoder.height);
+      let rgba = new Uint8Array(imageData.data.buffer);
+      encoder.addFrameRgba(rgba);
+    };
+  }
+}
+
+
+
+function finalizarGrabación() {
+  recording = false
+  console.log('recording stopped')
+
+  encoder.finalize()
+  const uint8Array = encoder.FS.readFile(encoder.outputFilename);
+  const anchor = document.createElement('a')
+  anchor.href = URL.createObjectURL(new Blob([uint8Array], { type: 'video/mp4' }))
+  anchor.download = encoder.outputFilename
+  anchor.click()
+  encoder.delete()
+
+
+  preload()
+  window.location.reload();
+
 }
 
